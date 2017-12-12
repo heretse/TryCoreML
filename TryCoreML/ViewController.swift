706ef9fb32d67df368b8effecbbba68cd7cce1d4
winston_hsieh <winston_hsieh@gemteks.com>
2017-12-12 22:40:40 +0800

    Initial Commit

---
+++ TryCoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryCoreML
+//
+//  Created by Winston Hsieh on 12/12/2017.
+//  Copyright © 2017 Winston Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
